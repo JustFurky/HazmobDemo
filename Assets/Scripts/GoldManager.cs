2aa366f1f5f4f675b494adce1bca4722153ca87a
JustFurky <mahmut.topcu1997@gmail.com>
2022-08-04 13:18:50 +0300

    Final
    Final

--- Assets/Scripts/GoldManager.cs
+++ Assets/Scripts/GoldManager.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class GoldManager : MonoBehaviour
 {
     public int GoldValue;
+
+    [SerializeField] GameObject _coinPrefab;
     [SerializeField] private PlayFabManager playFabManager;
     [SerializeField] private BallManager _ballManager;
+    [SerializeField]GameObject _lastCoin;
+    Vector2 _lastCoinPos;
+
     public void SetListeners()
     {
         _ballManager.GetCurrentBallScript().AddGold += addGold;
@@ -12,6 +17,10 @@ public class GoldManager : MonoBehaviour
 
     private void addGold()
     {
+        _lastCoinPos = _lastCoin.transform.position;
+        Destroy(_lastCoin);
+        _lastCoin = Instantiate(_coinPrefab);
+        _lastCoin.transform.position = new Vector2(Random.Range(-2, 2), Random.Range(_lastCoinPos.y + 4, _lastCoinPos.y + 7));
         GoldValue += 10;
     }
 }
